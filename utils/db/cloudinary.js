77fc31d1367fcfefb6cd2b7679e49d7270b8c007
jerry <jerrygonzalezzambrano@gmail.com>
2023-08-07 17:39:29 +0200

    uploads images

--- utils/db/cloudinary.js
+++ utils/db/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
 cloudinary.config({ 
-  cloud_name: 'dgesuhk6r', 
-  api_key: '934675915129934', 
-  api_secret: '5WD7gCpkUE4tbEWBl5Gd7dy8_ao'
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET
 });
 
 module.exports = cloudinary
